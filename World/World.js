8a0e61d6fa1b925b8335938a6cdeb5939d6a20b9
Keshav Upadhyaya <k.upadhyaya@l3bm.com>
2023-12-05 09:17:25 +0100

    mesh group

--- World/World.js
+++ World/World.js
@@ -1,6 +1,6 @@
 import { createCamera } from "../components/camera";
-import { createCube } from "../components/cube";
 import { createLights } from "../components/lights";
+import { createMeshGroup } from "../components/meshGroup";
 import { createScene } from "../components/scene";
 import { createControls } from "../systems/controls";
 import { Loop } from "../systems/Loop";
@@ -8,54 +8,53 @@ import { createRenderer } from "../systems/renderer";
 import { ReSizer } from "../systems/Resizer";
 
 class World {
-    // 1. Create an instance of the World app
+  // 1. Create an instance of the World app
 
-    // ideally these should be private... maybe typescript is better?
-    constructor(container) {
-        this.camera = createCamera();
-        this.scene = createScene();
-        this.renderer = createRenderer();
-        this.loop = new Loop(this.camera, this.scene, this.renderer);
+  // ideally these should be private... maybe typescript is better?
+  constructor(container) {
+    this.camera = createCamera();
+    this.scene = createScene();
+    this.renderer = createRenderer();
+    this.loop = new Loop(this.camera, this.scene, this.renderer);
 
-        container.append(this.renderer.domElement);
+    container.append(this.renderer.domElement);
 
-        const controls = createControls(this.camera, this.renderer.domElement )
+    const controls = createControls(this.camera, this.renderer.domElement);
 
+    const meshGroup = createMeshGroup();
+    const { ambientLight, light } = createLights();
 
-        const cube = createCube();
-        const {ambientLight, light} = createLights();
+    // controls.target.copy(cube.position);
 
-        // controls.target.copy(cube.position);
+    controls.addEventListener("change", () => {
+      this.render();
+    });
 
-        controls.addEventListener('change', () => {
-            this.render();
-        })
+    this.loop.updatableObjs.push(controls, meshGroup);
+    // this.loop.updatableObjs.push(cube);
 
-        this.loop.updatableObjs.push(controls)
-        // this.loop.updatableObjs.push(cube);
+    this.scene.add(meshGroup, light, ambientLight);
 
-        this.scene.add(cube, light, ambientLight);
+    const resizer = new ReSizer(container, this.camera, this.renderer);
 
-        const resizer = new ReSizer(container, this.camera, this.renderer);
+    // not required since loop animation will take care of rerendering (it's fast enough)
+    resizer.onResize = () => {
+      this.render();
+    };
+  }
 
-        // not required since loop animation will take care of rerendering (it's fast enough)
-        resizer.onResize = () => {
-            this.render();
-        }
-    }
+  // 2. Render the scene
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
 
-    // 2. Render the scene
-    render() {
-        this.renderer.render(this.scene, this.camera);
-    }
+  start() {
+    this.loop.start();
+  }
 
-    start() {
-        this.loop.start();
-    }
-
-    stop() {
-        this.loop.stop();
-    }
+  stop() {
+    this.loop.stop();
+  }
 }
 
 export { World };
--- components/lights.js
+++ components/lights.js
@@ -1,14 +1,13 @@
 import { DirectionalLight, HemisphereLight } from "three";
 
 function createLights() {
-    const  ambientLight = 
-    new HemisphereLight('white', 'darkslategrey', 5);
-    // new AmbientLight('white', 2);
-    
-    const light = new DirectionalLight('white', 8);
+  const ambientLight = new HemisphereLight("white", "darkslategrey", 10);
+  // new AmbientLight('white', 2);
 
-    light.position.set(10, 10, 10)
-    return {ambientLight, light}
+  const light = new DirectionalLight("white", 5);
+
+  light.position.set(10, 10, 10);
+  return { ambientLight, light };
 }
 
 export { createLights };
